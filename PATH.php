b536ba6623084b1482eb6a0d953cd57806834fc7
krlos <carloscharlis_16@hotmail.com>
2016-06-28 22:52:29 -0500

    verificacion de path en heroku

--- PATH.php
+++ PATH.php
@@ -1,6 +1,26 @@
 <?php
 
 	//$PATH = $_SERVER["DOCUMENT_ROOT"]."/BienestarCun/";
-	$PATH = $_SERVER["DOCUMENT_ROOT"]."/bienestar-cun/";
+	$PATH = $_SERVER["DOCUMENT_ROOT"];
+
+	function listar_directorios_ruta($PATH){ 
+   // abrir un directorio y listarlo recursivo 
+   if (is_dir($ruta)) { 
+      if ($dh = opendir($ruta)) { 
+         while (($file = readdir($dh)) !== false) { 
+            //esta línea la utilizaríamos si queremos listar todo lo que hay en el directorio 
+            //mostraría tanto archivos como directorios 
+            //echo "<br>Nombre de archivo: $file : Es un: " . filetype($ruta . $file); 
+            if (is_dir($ruta . $file) && $file!="." && $file!=".."){ 
+               //solo si el archivo es un directorio, distinto que "." y ".." 
+               echo "<br>Directorio: $ruta$file"; 
+               listar_directorios_ruta($ruta . $file . "/"); 
+            } 
+         } 
+      closedir($dh); 
+      } 
+   }else 
+      echo "<br>No es ruta valida"; 
+}
 	
 ?>
